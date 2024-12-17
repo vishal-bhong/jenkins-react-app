62baa51c796206283935260d0910c17979ef5b22
vishal-bhong <vishalbhong9803@gmail.com>
2024-12-17 21:57:29 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello from react app</h1>
+      <h1>Hello from react app. Now go to hell :( </h1>
     </div>
   );
 }
